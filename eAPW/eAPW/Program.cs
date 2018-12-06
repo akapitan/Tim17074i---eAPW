3d78d06d91a9e24c7f22836a13501a465e1c4071
akapitan <aleksandarkapitan@gmail.com>
2018-12-06 05:59:10 +0100

    Program.cs

--- eAPW/eAPW/Program.cs
+++ eAPW/eAPW/Program.cs
@@ -16,7 +16,7 @@ namespace eAPW
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmPrijava());
         }
     }
 }
