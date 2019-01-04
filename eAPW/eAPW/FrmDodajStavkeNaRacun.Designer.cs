d7a075e131e2b14d8825c20a8382fb12acc11e79
akapitan <aleksandarkapitan@gmail.com>
2019-01-04 22:50:47 +0100

    Implementirana forma za dodavanje stavke na racun

--- eAPW/eAPW/FrmDodajStavkeNaRacun.Designer.cs
+++ eAPW/eAPW/FrmDodajStavkeNaRacun.Designer.cs
@@ -28,11 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSelektiraniNaziv = new System.Windows.Forms.TextBox();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.btnOdustani = new System.Windows.Forms.Button();
+            this.txtPretraga = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvPopisStavki = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPopisStavki)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtSelektiraniNaziv);
+            this.groupBox1.Controls.Add(this.btnDodaj);
+            this.groupBox1.Location = new System.Drawing.Point(25, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(596, 60);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Selektirani predmet";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(312, 30);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(127, 22);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(248, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Količina";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Naziv";
+            // 
+            // txtSelektiraniNaziv
+            // 
+            this.txtSelektiraniNaziv.Location = new System.Drawing.Point(69, 25);
+            this.txtSelektiraniNaziv.Name = "txtSelektiraniNaziv";
+            this.txtSelektiraniNaziv.ReadOnly = true;
+            this.txtSelektiraniNaziv.Size = new System.Drawing.Size(162, 22);
+            this.txtSelektiraniNaziv.TabIndex = 2;
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Location = new System.Drawing.Point(474, 21);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(92, 35);
+            this.btnDodaj.TabIndex = 3;
+            this.btnDodaj.Text = "Dodaj";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            this.btnDodaj.Click += new System.EventHandler(this.btnDodaj_Click);
+            // 
+            // btnOdustani
+            // 
+            this.btnOdustani.Location = new System.Drawing.Point(25, 529);
+            this.btnOdustani.Name = "btnOdustani";
+            this.btnOdustani.Size = new System.Drawing.Size(92, 35);
+            this.btnOdustani.TabIndex = 8;
+            this.btnOdustani.Text = "Natrag";
+            this.btnOdustani.UseVisualStyleBackColor = true;
+            this.btnOdustani.Click += new System.EventHandler(this.btnOdustani_Click);
+            // 
+            // txtPretraga
+            // 
+            this.txtPretraga.Location = new System.Drawing.Point(92, 23);
+            this.txtPretraga.Name = "txtPretraga";
+            this.txtPretraga.Size = new System.Drawing.Size(162, 22);
+            this.txtPretraga.TabIndex = 7;
+            this.txtPretraga.TextChanged += new System.EventHandler(this.txtPretraga_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Pretraga";
+            // 
+            // dgvPopisStavki
+            // 
+            this.dgvPopisStavki.AllowUserToAddRows = false;
+            this.dgvPopisStavki.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPopisStavki.Location = new System.Drawing.Point(25, 139);
+            this.dgvPopisStavki.Name = "dgvPopisStavki";
+            this.dgvPopisStavki.ReadOnly = true;
+            this.dgvPopisStavki.RowTemplate.Height = 24;
+            this.dgvPopisStavki.Size = new System.Drawing.Size(596, 347);
+            this.dgvPopisStavki.TabIndex = 5;
+            this.dgvPopisStavki.SelectionChanged += new System.EventHandler(this.dgvPopisStavki_SelectionChanged);
+            // 
+            // FrmDodajStavkeNaRacun
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 606);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnOdustani);
+            this.Controls.Add(this.txtPretraga);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvPopisStavki);
+            this.Name = "FrmDodajStavkeNaRacun";
             this.Text = "FrmDodajStavkeNaRacun";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPopisStavki)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSelektiraniNaziv;
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.Button btnOdustani;
+        private System.Windows.Forms.TextBox txtPretraga;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvPopisStavki;
     }
 }
--- eAPW/eAPW/FrmDodajStavkeNaRacun.cs
+++ eAPW/eAPW/FrmDodajStavkeNaRacun.cs
@@ -16,5 +16,112 @@ namespace eAPW
         {
             InitializeComponent();
         }
+
+        private void ispisSvihProizvoda()
+        {
+
+            using (var db = new ProgramskoInzenjerstvoDBEntities())
+            {
+                List<Djelovi> listDjelovi = new List<Djelovi>();
+                dgvPopisStavki.DataSource = null;
+
+                foreach (Djelovi z in db.Djelovis)
+                {
+                    //z.kate = z.Kategorija1.naziv;
+                    listDjelovi.Add(z);
+                }
+
+                dgvPopisStavki.DataSource = null;
+                dgvPopisStavki.DataSource = new BindingSource(listDjelovi, null);
+
+                dgvPopisStavki.Columns["Kategorija1"].Visible = false;
+                dgvPopisStavki.Columns["Model_vozila"].Visible = false;
+                dgvPopisStavki.Columns["Proizvodac"].Visible = false;
+                dgvPopisStavki.Columns["Narudzba_has_Djelovi"].Visible = false;
+                dgvPopisStavki.Columns["Racun_Has_Djelovi"].Visible = false;
+                dgvPopisStavki.Columns["Rezervacija_has_Djelovi"].Visible = false;
+            }
+
+        }
+        private void ispisSvihProizvoda(string textPretraga)
+        {
+            using (var db = new ProgramskoInzenjerstvoDBEntities())
+            {
+                List<Djelovi> listDjelovi = new List<Djelovi>();
+
+                foreach (Djelovi z in db.Djelovis)
+                {
+
+                    if (z.naziv.ToLower().Contains(textPretraga) && z != null)
+                    {
+                        //z.kate = z.Kategorija1.naziv;
+                        listDjelovi.Add(z);
+                    }
+
+                }
+                dgvPopisStavki.DataSource = null;
+                dgvPopisStavki.DataSource = new BindingSource(listDjelovi, null);
+
+                dgvPopisStavki.Columns["Kategorija1"].Visible = false;
+                dgvPopisStavki.Columns["Model_vozila"].Visible = false;
+                dgvPopisStavki.Columns["Proizvodac"].Visible = false;
+                dgvPopisStavki.Columns["Narudzba_has_Djelovi"].Visible = false;
+                dgvPopisStavki.Columns["Racun_Has_Djelovi"].Visible = false;
+                dgvPopisStavki.Columns["Rezervacija_has_Djelovi"].Visible = false;
+            }
+        }
+
+        private void btnOdustani_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtPretraga_TextChanged(object sender, EventArgs e)
+        {
+            ispisSvihProizvoda(txtPretraga.Text);
+        }
+        Djelovi selektirani;
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            if (selektirani != null)
+            {
+                if (selektirani.kolicina > 0)
+                {
+                    if (FrmRacunNoviMaloprodaja.bl.Any(x => x.id == selektirani.id && x.naziv == selektirani.naziv))
+                    {
+                        MessageBox.Show("Proizvod je već dodan na račun");
+                    }
+                    else
+                    {
+                        FrmRacunNoviMaloprodaja.bl.Add(selektirani);
+                    }
+                }
+
+                else
+                {
+                    MessageBox.Show("Tog proizvoda nema na skladištu");
+                }
+
+            }
+        }
+
+        private void dgvPopisStavki_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                selektirani = dgvPopisStavki.CurrentRow.DataBoundItem as Djelovi;
+                if (selektirani != null)
+                {
+                    txtSelektiraniNaziv.Text = selektirani.naziv;
+                    numericUpDown1.Maximum = selektirani.kolicina;
+                    numericUpDown1.Value = selektirani.kolicina;
+                }
+            }
+            catch
+            {
+
+            }
+        }
     }
 }
